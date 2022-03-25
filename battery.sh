31ec6f105b7c297e7705af6a8ae2094c815e7b4c
gorkalertxundi <gorka.lertxundi@alumni.mondragon.edu>
2022-03-25 09:55:51 +0100

    notify title added

--- battery.sh
+++ battery.sh
@@ -4,7 +4,7 @@ battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
 if [[ $battery_level -lt 20 ]]
 then
-  notify-send -u critical "Low battery level: ${battery_level}%   Please charge the computer";
+  notify-send -u critical "Low battery!" "battery level: ${battery_level}%   Please charge the computer";
 fi
 
 echo $(acpi -b)
